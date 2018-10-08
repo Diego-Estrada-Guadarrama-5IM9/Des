b251bbdda722897779f605d4c2dd436d847256da
IkoDieko <diego_estrada14@hotmail.com>
2018-10-07 23:28:48 -0500

    Ya está dividio en métodos

--- src/des/Des.java
+++ src/des/Des.java
@@ -12,9 +12,62 @@ public class Des  {
   String Kf[] = new String [17];
   String D[] = new String [17];
    
+  //Método que realiza el DES
    public void Des(/*String llave*/){
-	   //inicializamos C0
-        String C0 = Character.toString(llave.charAt(56)) + Character.toString(llave.charAt(48)) +
+       Rondas(llave);
+       Llaves();
+      String PID = Character.toString(dato.charAt(57)) + Character.toString(dato.charAt(49)) + Character.toString(dato.charAt(41)) + Character.toString(dato.charAt(33)) +
+      Character.toString(dato.charAt(25)) + Character.toString(dato.charAt(17)) + Character.toString(dato.charAt(9)) + Character.toString(dato.charAt(1)) +
+                    
+     Character.toString(dato.charAt(59)) + Character.toString(dato.charAt(51)) + Character.toString(dato.charAt(43)) + Character.toString(dato.charAt(35)) +
+     Character.toString(dato.charAt(27)) + Character.toString(dato.charAt(19)) + Character.toString(dato.charAt(11)) + Character.toString(dato.charAt(3)) +
+                    
+     Character.toString(dato.charAt(61)) + Character.toString(dato.charAt(53)) + Character.toString(dato.charAt(45)) + Character.toString(dato.charAt(37)) +
+     Character.toString(dato.charAt(29)) + Character.toString(dato.charAt(21)) + Character.toString(dato.charAt(13)) + Character.toString(dato.charAt(5)) +
+                    
+     Character.toString(dato.charAt(63)) + Character.toString(dato.charAt(55)) + Character.toString(dato.charAt(47)) + Character.toString(dato.charAt(39)) +
+     Character.toString(dato.charAt(31)) + Character.toString(dato.charAt(23)) + Character.toString(dato.charAt(15)) + Character.toString(dato.charAt(7)) +
+                    
+     Character.toString(dato.charAt(56)) + Character.toString(dato.charAt(48)) + Character.toString(dato.charAt(40)) + Character.toString(dato.charAt(32)) +
+     Character.toString(dato.charAt(24)) + Character.toString(dato.charAt(16)) + Character.toString(dato.charAt(8)) + Character.toString(dato.charAt(0)) +
+                    
+     Character.toString(dato.charAt(58)) + Character.toString(dato.charAt(50)) + Character.toString(dato.charAt(42)) + Character.toString(dato.charAt(34)) +
+     Character.toString(dato.charAt(26)) + Character.toString(dato.charAt(18)) + Character.toString(dato.charAt(10)) + Character.toString(dato.charAt(2)) +
+                    
+     Character.toString(dato.charAt(60)) + Character.toString(dato.charAt(52)) + Character.toString(dato.charAt(44)) + Character.toString(dato.charAt(36)) +
+     Character.toString(dato.charAt(28)) + Character.toString(dato.charAt(20)) + Character.toString(dato.charAt(12)) + Character.toString(dato.charAt(4)) +
+                    
+     Character.toString(dato.charAt(62)) + Character.toString(dato.charAt(54)) + Character.toString(dato.charAt(46)) + Character.toString(dato.charAt(38)) +
+     Character.toString(dato.charAt(10)) + Character.toString(dato.charAt(22)) + Character.toString(dato.charAt(14)) + Character.toString(dato.charAt(6));
+            
+    }
+   
+   //Método que hace un Xor entre dos palabras
+   public void Xor(String P1, String P2){
+       String RXor;
+        char RPrueba[] = new char [P1.length()];
+        
+       for (int i = 0; i < P1.length(); i++) {
+           if(P1.charAt(i) =='0' && P2.charAt(i) == '0'){
+               RPrueba[i] = '0';
+           }
+           else if(P1.charAt(i) =='1' && P2.charAt(i) == '1'){
+               RPrueba[i] = '0';
+           } 
+           else if (P1.charAt(i) =='0' && P2.charAt(i) == '1'){
+               RPrueba[i] = '1';
+           }
+           else if (P1.charAt(i) =='1' && P2.charAt(i) == '0'){
+               RPrueba[i] = '1';
+           }
+           RXor = Character.toString(RPrueba[i]);
+           System.out.print(RXor);
+       }
+   }
+   
+   //Método que hace los desplazamientos de las rondas
+   public void Rondas(String llave){
+               String C0 = Character.toString(llave.charAt(56)) + Character.toString(llave.charAt(48)) +
         Character.toString(llave.charAt(40)) + Character.toString(llave.charAt(32)) +
         Character.toString(llave.charAt(24)) + Character.toString(llave.charAt(16)) +
         Character.toString(llave.charAt(8)) + Character.toString(llave.charAt(0)) +    
@@ -68,8 +121,11 @@ public class Des  {
             }
            // System.out.println("C"+i+"=" +C[i]+" D"+i+"= "+D[i]);
        }
-	   
-        for (int i = 1; i <= 16; i++) {
+   }
+   
+   //Método que nos crea las llaves (las K)
+   public void Llaves(){
+               for (int i = 1; i <= 16; i++) {
 			//ki es la union de la C y la D, mientras que kf será la permutacion de la Ki
             Ki[i] = C[i] + D[i];
             Kf[i] = Character.toString(Ki[i].charAt(13)) + Character.toString(Ki[i].charAt(16)) +
@@ -104,54 +160,67 @@ public class Des  {
             //System.out.println("K"+i+"= "+Kf[i]);
 
        }
-          //Se hace la permutación del dato
-            String PID = Character.toString(dato.charAt(57)) + Character.toString(dato.charAt(49)) + Character.toString(dato.charAt(41)) + Character.toString(dato.charAt(33)) +
-                    Character.toString(dato.charAt(25)) + Character.toString(dato.charAt(17)) + Character.toString(dato.charAt(9)) + Character.toString(dato.charAt(1)) +
-                    
-                    Character.toString(dato.charAt(59)) + Character.toString(dato.charAt(51)) + Character.toString(dato.charAt(43)) + Character.toString(dato.charAt(35)) +
-                    Character.toString(dato.charAt(27)) + Character.toString(dato.charAt(19)) + Character.toString(dato.charAt(11)) + Character.toString(dato.charAt(3)) +
-                    
-                    Character.toString(dato.charAt(61)) + Character.toString(dato.charAt(53)) + Character.toString(dato.charAt(45)) + Character.toString(dato.charAt(37)) +
-                    Character.toString(dato.charAt(29)) + Character.toString(dato.charAt(21)) + Character.toString(dato.charAt(13)) + Character.toString(dato.charAt(5)) +
-                    
-                    Character.toString(dato.charAt(63)) + Character.toString(dato.charAt(55)) + Character.toString(dato.charAt(47)) + Character.toString(dato.charAt(39)) +
-                    Character.toString(dato.charAt(31)) + Character.toString(dato.charAt(23)) + Character.toString(dato.charAt(15)) + Character.toString(dato.charAt(7)) +
-                    
-                    Character.toString(dato.charAt(56)) + Character.toString(dato.charAt(48)) + Character.toString(dato.charAt(40)) + Character.toString(dato.charAt(32)) +
-                    Character.toString(dato.charAt(24)) + Character.toString(dato.charAt(16)) + Character.toString(dato.charAt(8)) + Character.toString(dato.charAt(0)) +
-                    
-                    Character.toString(dato.charAt(58)) + Character.toString(dato.charAt(50)) + Character.toString(dato.charAt(42)) + Character.toString(dato.charAt(34)) +
-                    Character.toString(dato.charAt(26)) + Character.toString(dato.charAt(18)) + Character.toString(dato.charAt(10)) + Character.toString(dato.charAt(2)) +
-                    
-                    Character.toString(dato.charAt(60)) + Character.toString(dato.charAt(52)) + Character.toString(dato.charAt(44)) + Character.toString(dato.charAt(36)) +
-                    Character.toString(dato.charAt(28)) + Character.toString(dato.charAt(20)) + Character.toString(dato.charAt(12)) + Character.toString(dato.charAt(4)) +
-                    
-                    Character.toString(dato.charAt(62)) + Character.toString(dato.charAt(54)) + Character.toString(dato.charAt(46)) + Character.toString(dato.charAt(38)) +
-                    Character.toString(dato.charAt(10)) + Character.toString(dato.charAt(22)) + Character.toString(dato.charAt(14)) + Character.toString(dato.charAt(6));
-            
-            //System.out.println(PID);
-            
-    }
+   }
    
-   public void Xor(String P1, String P2){
-       String RXor;
-        char RPrueba[] = new char [P1.length()];
-        
-       for (int i = 0; i < P1.length(); i++) {
-           if(P1.charAt(i) =='0' && P2.charAt(i) == '0'){
-               RPrueba[i] = '0';
-           }
-           else if(P1.charAt(i) =='1' && P2.charAt(i) == '1'){
-               RPrueba[i] = '0';
-           } 
-           else if (P1.charAt(i) =='0' && P2.charAt(i) == '1'){
-               RPrueba[i] = '1';
-           }
-           else if (P1.charAt(i) =='1' && P2.charAt(i) == '0'){
-               RPrueba[i] = '1';
-           }
-           RXor = Character.toString(RPrueba[i]);
-           System.out.print(RXor);
-       }
+   //Método que realiza la expansión correspondiente
+   public void Expansion(String exp){
+           exp = Character.toString(exp.charAt(31)) + Character.toString(exp.charAt(0)) +
+                Character.toString(exp.charAt(1)) + Character.toString(exp.charAt(2)) +
+                Character.toString(exp.charAt(3)) + Character.toString(exp.charAt(4)) +
+                Character.toString(exp.charAt(3)) + Character.toString(exp.charAt(4)) +
+                   
+                Character.toString(exp.charAt(5)) + Character.toString(exp.charAt(6)) +
+                Character.toString(exp.charAt(7)) + Character.toString(exp.charAt(8)) +
+                Character.toString(exp.charAt(7)) + Character.toString(exp.charAt(8)) +
+                Character.toString(exp.charAt(9)) + Character.toString(exp.charAt(10)) +
+                   
+                Character.toString(exp.charAt(11)) + Character.toString(exp.charAt(12)) +
+                Character.toString(exp.charAt(11)) + Character.toString(exp.charAt(12)) +
+                Character.toString(exp.charAt(13)) + Character.toString(exp.charAt(14)) +
+                Character.toString(exp.charAt(15)) + Character.toString(exp.charAt(16)) +
+                   
+                 Character.toString(exp.charAt(15)) + Character.toString(exp.charAt(16)) +
+                 Character.toString(exp.charAt(17)) + Character.toString(exp.charAt(18)) +
+                 Character.toString(exp.charAt(19)) + Character.toString(exp.charAt(20)) +
+                 Character.toString(exp.charAt(19)) + Character.toString(exp.charAt(20)) +
+                   
+                 Character.toString(exp.charAt(21)) + Character.toString(exp.charAt(22)) +
+                 Character.toString(exp.charAt(23)) + Character.toString(exp.charAt(24)) +
+                 Character.toString(exp.charAt(23)) + Character.toString(exp.charAt(24)) +
+                 Character.toString(exp.charAt(25)) + Character.toString(exp.charAt(26)) +
+                   
+                 Character.toString(exp.charAt(27)) + Character.toString(exp.charAt(28)) +
+                 Character.toString(exp.charAt(27)) + Character.toString(exp.charAt(28)) +
+                 Character.toString(exp.charAt(29)) + Character.toString(exp.charAt(30)) +
+                 Character.toString(exp.charAt(31)) + Character.toString(exp.charAt(0));
+   }
+   
+   //Método que hace la permutación correspondiente
+   public void Permutacion(String per){
+       per = Character.toString(per.charAt(15)) + Character.toString(per.charAt(6)) +
+             Character.toString(per.charAt(19)) + Character.toString(per.charAt(20)) +
+             Character.toString(per.charAt(28)) + Character.toString(per.charAt(11)) +
+             Character.toString(per.charAt(27)) + Character.toString(per.charAt(16)) +
+               
+             Character.toString(per.charAt(0)) + Character.toString(per.charAt(14)) +
+             Character.toString(per.charAt(22)) + Character.toString(per.charAt(25)) +
+             Character.toString(per.charAt(4)) + Character.toString(per.charAt(17)) +
+             Character.toString(per.charAt(30)) + Character.toString(per.charAt(9)) +
+               
+             Character.toString(per.charAt(1)) + Character.toString(per.charAt(7)) +
+             Character.toString(per.charAt(23)) + Character.toString(per.charAt(25)) +
+             Character.toString(per.charAt(4)) + Character.toString(per.charAt(17)) +
+             Character.toString(per.charAt(30)) + Character.toString(per.charAt(9)) +
+               
+             Character.toString(per.charAt(18)) + Character.toString(per.charAt(12)) +
+             Character.toString(per.charAt(29)) + Character.toString(per.charAt(5)) +
+             Character.toString(per.charAt(21)) + Character.toString(per.charAt(10)) +
+             Character.toString(per.charAt(3)) + Character.toString(per.charAt(24));
+   }
+   
+   //Método que crea el funcionamiento de las 8 S
+   public void Sss (String ss){
+       
    }
 }
+    
